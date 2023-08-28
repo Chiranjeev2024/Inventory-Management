be806351dd0c65ebfa5aa6636cf2566f56cc5f00
ssuyash3000 <ssuyash2001@gmail.com>
2023-08-28 19:26:05 +0530

    adds ejs and ejs layouts to make the views dynamic

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import express from "express";
 import ProductController from "./src/controller/product.controller.js";
-
+import path from "path";
+import ejsLayouts from "express-ejs-layouts";
 const server = express();
+server.set("view engine", "ejs");
+
+const pathOfviews = path.join(path.resolve(), "src", "views");
+server.set("views", pathOfviews);
+server.use(ejsLayouts);
 
 const productController = new ProductController();
 
--- src/controller/product.controller.js
+++ src/controller/product.controller.js
@@ -1,6 +1,9 @@
 import path from "path"; //core module
+import ProductModel from "../model/product.model.js";
 export default class ProductController {
   getProducts(req, res) {
+    let productArray = ProductModel.get();
+    console.log(productArray);
     //In sendFile we need to pass the path of the file
     //We will generate file path using path module
     //path.resolve() gives the path of the current directory - of the file which is
@@ -12,6 +15,7 @@ export default class ProductController {
       "views",
       "products.html"
     );
-    return res.sendFile(pathOfFile); //.sendFile () comes from express to the res obejct
+    // return res.sendFile(pathOfFile); //.sendFile () comes from express to the res obejct
+    return res.render("products.ejs", { products: productArray });
   }
 }
