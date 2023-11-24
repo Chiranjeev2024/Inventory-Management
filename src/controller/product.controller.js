037051da9518fc912e51013e8fa8e84d3466e128
ssuyash3000 <ssuyash2001@gmail.com>
2023-11-25 01:41:29 +0530

    adds server side  validation in add-product form

--- src/controller/product.controller.js
+++ src/controller/product.controller.js
@@ -19,9 +19,33 @@ export default class ProductController {
     return res.render("products.ejs", { products: productArray });
   }
   getAddForm(req, res) {
-    res.render("add-product", {});
+    res.render("add-product", { errorMessage: null });
   }
   addNewProduct(req, res) {
+    //data validation
+    const { name, price, desc, imageUrl } = req.body;
+    let errors = [];
+    if (!name || name.trim() == "") {
+      // if name is undefined or null or empty | .trim() method removes space from beginning and end of the string
+      errors.push("a valid name is required ");
+    }
+    if (!price || parseFloat(price) < 1) {
+      errors.push("Price must be a greater than 0");
+    }
+    //For checking URL we can use URL() parser which is in-built feature of JS
+    //If the string passed to this parser is not a valid URL then it throws error
+    try {
+      const validURL = new URL(imageUrl);
+    } catch (err) {
+      errors.push("URL is not valid");
+    }
+
+    //If we even one error is found we will render the form page with errorMessage
+    if (errors.length > 0) {
+      //We are sending only the first error message to the view
+      return res.render("add-product", { errorMessage: errors[0] });
+    }
+
     //accessing the form data
     console.log(req.body);
     ProductModel.add(req.body);
