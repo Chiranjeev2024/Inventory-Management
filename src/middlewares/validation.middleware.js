490d6e287910e14c24e65f9da92391c45630f491
ssuyash3000 <ssuyash2001@gmail.com>
2023-12-10 13:47:43 +0530

    implements validation using express-validator

--- src/middlewares/validation.middleware.js
+++ src/middlewares/validation.middleware.js
@@ -1,26 +1,37 @@
-export const validateRequest = (req, res, next) => {
-  const { name, price, desc, imageUrl } = req.body;
-  let errors = [];
-  if (!name || name.trim() == "") {
-    // if name is undefined or null or empty | .trim() method removes space from beginning and end of the string
-    errors.push("a valid name is required ");
-  }
-  if (!price || parseFloat(price) < 1) {
-    errors.push("Price must be a greater than 0");
-  }
-  //For checking URL we can use URL() parser which is in-built feature of JS
-  //If the string passed to this parser is not a valid URL then it throws error
-  try {
-    const validURL = new URL(imageUrl);
-  } catch (err) {
-    errors.push("URL is not valid");
-  }
+import { body, validationResult } from "express-validator";
+export const validateRequest = async (req, res, next) => {
+  console.log("validator called");
+
+  //1. Steup rule for validation using express-validator
+  const rules = [
+    //For validating name we will use isEmpty method
+    body("name").notEmpty().withMessage("Name is required"),
+    //For validating price we using isFloat method, in which we pass rule as
+    //gt: 0 where "gt" stands for greater than
+    body("price")
+      .isFloat({ gt: 0 })
+      .withMessage("Price should be greater than 0"),
+    body("imageUrl").isURL().withMessage("Invalid URL"),
+  ];
+
+  //2. Running the rules
+  //.all method takes an array of promies and runs them all
+  //Every rule have run function and it takes request object as parameter
+  await Promise.all(rules.map((rule) => rule.run(req)));
+
+  //3. Checking for errors
+  let validationError = validationResult(req);
+
   //If we even one error is found we will render the form page with errorMessage
-  if (errors.length > 0) {
+  if (!validationError.isEmpty()) {
     //We are sending only the first error message to the view
-    return res.render("add-product", { errorMessage: errors[0] });
+    return res.render("add-product", {
+      //validationError is an object returned by the validationResult
+      errorMessage: validationError.array()[0].msg,
+    });
     //return keyword is used here show that code below do not executes
   }
+
   //calling next middleware in the pipeline
   next();
 };
